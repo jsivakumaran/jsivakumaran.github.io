7d9c17af6a900efacd4fe3ea9c0911dc6b78cd57
Gabe Finch and Jay Sivakumaran <gabefinch@gmail.com>
2015-02-26 14:14:30 -0800

    Add comment partial

--- controllers/CommentCtrl.js
+++ controllers/CommentCtrl.js
@@ -1,4 +1,14 @@
-slackerNews.controller('CommentCtrl', ['$scope','$state',
-function CommentCtrl(){
+slackerNews.controller('CommentCtrl', ['$scope','$state','$stateParams', 'LinkFactory', 'UtilityFactory',
+function CommentCtrl($scope, $state, $stateParams, LinkFactory, UtilityFactory){
 
-}])
+  $scope.links = LinkFactory.links;
+  $scope.LinkFactory = LinkFactory;
+  $scope.link = UtilityFactory.findById(LinkFactory.links, $stateParams.linkId);
+
+  $scope.addThenHome = function(link){
+    LinkFactory.addComment(link);
+    $state.go('newsList');
+  }
+
+
+}]);
---
+++ services/UtilityFactory.js
@@ -0,0 +1,14 @@
+slackerNews.factory('UtilityFactory', function() {
+  factory = {};
+
+  factory.findById = function(collection, id) {
+    for (var i = 0; i < collection.length; i++) {
+      if (collection[i].id == id) {
+        return collection[i];
+      }
+    }
+    return null;
+  }
+
+  return factory;
+});
